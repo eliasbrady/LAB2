90a95a61398b61a59fe8b3960797dd2c6c3da143
Jesse Robert Finley <jrfinley42@students.tntech.edu>
2017-01-27 08:36:13 -0600

    Documentation added

--- ReadFile.h
+++ ReadFile.h
@@ -6,6 +6,11 @@
 #include <fstream>
 using namespace std;
 
+//! The class creates a write a file and writes to it.
+/*!
+ * The class accepts a character array through the constructor.
+ */
+ 
 class ReadFile
 {
 	private:
@@ -13,10 +18,15 @@ class ReadFile
 		bool _eof;
 		bool closed;
 	public:
+//! The constructor that accepts the character array to create and open a read file
 		ReadFile(const char* file_name);
+//! The destructor to close the read file from memory
 		~ReadFile();
+//! Retreives a string from the readfile
 		String* readLine();
+//! Checks the eof status of the readfile
 		bool eof();
+//! Closes the read file
 		void close();
 		
 };
--- WriteFile.h
+++ WriteFile.h
@@ -6,6 +6,11 @@
 #include <fstream>
 using namespace std;
 
+//! The class creates a write a file and writes to it.
+/*!
+ * The class accepts a character array through the constructor.
+ */
+
 class WriteFile
 {
     private:
@@ -13,10 +18,13 @@ class WriteFile
 	    bool closed;
 	   
 	public:
+//! The constructor that accepts the character array to create and open a write file
 		WriteFile(const char* file_name);
+//! The destructor to close and remove the write file from memory
 		~WriteFile();
-		
+//! Writes a string to the write file
 		void writeLine(String* line);
+//! Closes a writefile early or upon deconstruction of WriteFile
 		void close();
 };
 
